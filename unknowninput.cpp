55587ca37e19e020da98ef710622a9f79854a1db
tcchristopher <camilochristopher@gmail.com>
2025-09-18 10:23:44 +0200

    Continuing Chapter 1

---
+++ unknowninput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    int sum = 0, value = 0; //  we have to ensure that each variable is cleanly set to
+                            //  as the code struggles to recognise seperate variables otherwise
+    
+                            //  read until end-of-file, calculating a running total of all values read
+    while (std::cin >> value)
+    {
+        sum += value;   //  equivalent to sum = sum + value
+    }
+    std::cout << "Sum is: " << sum << std::endl;
+    return 0;
+}

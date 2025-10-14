7229861e29f2f863bf23405cec8232d0c54cfdd9
tcchristopher <camilochristopher@gmail.com>
2025-10-14 10:15:12 +0200

    Starting Chapter 3

---
+++ PPP/chapter2/exercises/exercisechap2.cpp
@@ -0,0 +1,11 @@
+//  This is coding exercise from PPP Pg 55 for Chapter 2
+//  This code will output two lines "Hello, programming!" and "Here we go!"
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, programming!\n";
+    std::cout << "Here we go!\n";
+    return 0;
+}

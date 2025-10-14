fbc9364a2084fffe6d2ec028a78b5b2b3d3208db
tcchristopher <camilochristopher@gmail.com>
2025-10-14 09:42:41 +0200

    Reading Chapter 2 of PPP

--- PPP/chapter2/helloworld.cpp
+++ PPP/chapter2/helloworld.cpp
@@ -3,11 +3,12 @@
 //  Interesting - Stroustrup actually recommends not using it as it flushes the stream, perhaps prematurely and expensively
 //  Nor iostream
 //  Okay so Stroustrup had a full custom library for standard includes, being "std_lib_facilities.h"
+//  DRILL TIME
 
 #include <iostream>
 
-int main()
+int main()      // C++ programs start by executing the function main
 {
-    std::cout << "Hello, World!\n";
+    std::cout << "Hello, World!\n";     // output "Hello, World!"
     return 0;
 }

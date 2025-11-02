0230fa3a7cfa02bcbfa4e8e293d3ca320c674ebf
tcchristopher <camilochristopher@gmail.com>
2025-11-02 17:45:06 +0200

    Continuing Chapter 7

--- PPP/chapter7/examples/ch7_exam7_8.cpp
+++ PPP/chapter7/examples/ch7_exam7_8.cpp
@@ -1,4 +1,4 @@
-// Cleaning up the code 
+// Variable clean up
 
 // The road to the final version of the full Calculator
 #include "std_lib_facilities.h"
@@ -10,21 +10,26 @@ const char quit = 'q';
 const char print = ';';
 const string prompt = "> ";
 const string result = "= ";
+const char name = 'a';                          // name token
+const char let = 'L';                           // declaration token
+const string declkey = "let";                   // declaration keyword
 
 
 // Declare classes for Calculator
-
-class Token{                                    // a very simple user-defined type
+// Tokens
+class Token{                                                    // a very simple user-defined type
 public:
-    char kind;                                  // what kind of token
-    double value;                               // for numbers; a value
-    Token(char k):kind{k}, value {0.0}{}            // construct from one value
-    Token(char k, double v):kind{k}, value{v}{}     // construct from two values
+    char kind;                                                  // what kind of token
+    double value;                                               // for numbers; a value
+    string name;
+    Token() : kind{0} {}                                        // default constructor
+    Token(char ch) : kind{ch} {}                                // initialize kind with ch
+    Token(char ch, double val) : kind{ch}, value{val} {}        // initialize kind and value
+    Token(char ch, string n) : kind{ch}, name{n} {}             // initialize kind and name
 };
 
 class Token_stream {
 public:
-    // Token_stream();                             // make a Token_stream that reads from std::cin
     Token get();                                // get a Token (get() is defined elsewhere)
     void putback(Token t);                      // put a Token back
     void ignore(char c);                        // discard characters up to and including a c
@@ -32,12 +37,6 @@ private:
     bool full{false};                           // is there a Token in the buffer?
     Token buffer{0};                            // here is where we keep a Token put back using putback()
 };
-
-// // The constructor just sets full to indicate that the bufffer is empty:
-// Token_stream::Token_stream()
-// :full(false), buffer(0)                         // no Token in buffer
-// {
-// }
 
 // The ignore function in Token_stream to clean up after errors are caught
 void Token_stream::ignore(char c)
@@ -73,9 +72,11 @@ Token Token_stream::get()
 
     char ch;
     std::cin >> ch;                             // ntoe that >> skips whitespace (space, newline, tab, etc.)
+    
     switch (ch){
     case quit:
     case print:
+    case '=':
     case '%':                                   // for modulo
     case '(': 
     case ')': 
@@ -94,35 +95,64 @@ Token Token_stream::get()
             std::cin.putback(ch);               // put digit back into the input stream
             double val;
             std::cin >> val;                    // read a floating-point number
-            return Token(number, val);             // let '8' represent "a number"            
+            return Token(number, val);          // let '8' represent "a number"            
         }
     default:
+        if (isalpha(ch)){
+            string s;
+            s += ch;
+            while (std::cin.get(ch) && (isalpha(ch) || isdigit(ch))) s+=ch;
+            std::cin.putback(ch);
+            if (s == declkey) return Token{let};    // declaration keyword
+            return Token{name,s};
+        }
         error("Bad token");
         return Token('?');                      // Never reached but satisfies compiler
     }
 }
 
-
 Token_stream ts;                                // provides functionality with get() and putback()
 
+// Variable Classes
+class Variable {
+public:
+    string name;
+    double value;
+};
 
-// function declarations
+// Vector of Variables (for storing such variables)
+vector<Variable> var_table;
+
+
+// Function declarations
 double expression();
 double term();
 double factorial();
 double primary();
 void calculate ();
 void clean_up_mess();
+double get_value (string s);
+void set_value(string s, double d);
+double statement();
+double declaration();
+bool is_declared(string var);
+double define_name(string var, double val);
 
-
+// Main Body
 int main()
 try{
+    // predefine names
+    define_name("pi", 3.1415926535);
+    define_name("e", 2.7182818284);
+
+    // Welcome message
     std::cout   << "Welcome to our simple calculator. Please enter expressions using floating-point numbers.\n"
                 << "You can use the following operators (+ - * /).\n"
                 << "Please use ! for factorial cases with a non-negative integer.\n"
                 << "Enter ';' to immediately print out the answer and 'q' to exit.\n";
 
     calculate();
+
     keep_window_open();
     return 0;
 }
@@ -138,6 +168,7 @@ catch(...){
     return 2;
 }
 
+// Ancilliary Functions
 void calculate ()                               // expression evaluation loop
 {
     while (std::cin)
@@ -145,14 +176,42 @@ void calculate ()                               // expression evaluation loop
         std::cout << prompt;
         Token t = ts.get();
         while (t.kind == print) t = ts.get();   // first discard all "prints"
-        if (t.kind == quit) return;
+        if (t.kind == quit) return;             // quit
         ts.putback(t);
-        std::cout << result << expression() << '\n';
+        std::cout << result << statement() << '\n';
     }
     catch (exception& e){
         std::cerr << e.what() << '\n';          // write an error message
         clean_up_mess();
     }
+}
+
+double statement()
+{
+    Token t = ts.get();
+    switch(t.kind){
+    case let:
+        return declaration();
+    default:
+        ts.putback(t);
+        return expression();
+
+    }
+}
+
+// Declare a variable called "name" with the initial value "expression"
+double declaration()
+{
+    Token t = ts.get();
+    if (t.kind != name) error("name expected in declaration");
+    string var_name = t.name;
+
+    Token t2 = ts.get();
+    if (t2.kind != '=') error("= missing in declaration of ", var_name);
+
+    double d = expression();
+    define_name(var_name, d);
+    return d;
 }
 
 void clean_up_mess()
@@ -240,7 +299,6 @@ double factorial()
     }
 }
 
-
 double primary()
 {
     Token t = ts.get();
@@ -259,8 +317,10 @@ double primary()
         if (t.kind != '}') error("'}' expected");
         return d;
     }
-    case number:                                   // we use '8' to represent a number
+    case number:                                // we use '8' to represent a number
         return t.value;                         // return the number's value
+    case name:
+        return get_value(t.name);
     case '-':
         return - primary();
     case '+':
@@ -269,4 +329,42 @@ double primary()
         error("primary expected");
         return 0;                               // Never reached but satisfies compiler
     }
+}
+
+// Return the value of the Variable named s
+double get_value (string s)
+{
+    for (const Variable& v : var_table){
+        if (v.name == s) return v.value;
+    }
+    error("get: undefined variable ", s);
+    return 0.0;
+}
+
+// Set the Variable named s to d
+void set_value(string s, double d)
+{
+    for (Variable& v : var_table){
+        if (v.name == s){
+            v.value = d;
+            return;
+        }
+    }
+    error("set: undefined variable ", s);
+}
+
+// Is var already in var_table?
+bool is_declared(string var)
+{
+    for (const Variable& v: var_table)
+        if (v.name == var) return true;
+    return false;
+}
+
+// Add {var, val} to var_table
+double define_name(string var, double val)
+{
+    if (is_declared(var)) error(var, "declared twice");
+    var_table.push_back(Variable{var,val});
+    return val;
 }
